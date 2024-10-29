9794749bc53f510fb9261c406d580ee8cf464ee5
LalitPro <140280rakesh@gmail.com>
2024-10-29 08:53:15 +0530

    Update Show.jsx

--- src/Show.jsx
+++ src/Show.jsx
@@ -6,7 +6,7 @@ function Show() {
     <div
       className="flex items-center justify-center w-screen h-screen text-center"
     >
-      <div className="relative z-10 flex flex-wrap items-center justify-center w-screen h-screen gap-20 ml-5 overflow-hidden text-2xl sm:text-5xl md:ml-20 sm:ml-10 sm:gap-40 md:gap-60">
+      <div className="bg-transparent relative z-10 flex flex-wrap items-center justify-center w-screen h-screen gap-20 ml-5 overflow-hidden text-2xl sm:text-5xl md:ml-20 sm:ml-10 sm:gap-40 md:gap-60">
         <h3 className="animate-ping">😊</h3>
         <h3 className="animate-ping">🔥</h3>
         <h3 className="animate-ping">🌟</h3>
